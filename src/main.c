bcfc3b8eda63c698ad27758f51855f3b672f4ea0
Villager Z <git.villagerz@pm.me>
2024-06-11 21:36:58 -0400

    14.3 dynamic array of instructions starting with OP_RETURN.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
-#include "common.h"
 #include <stdio.h>
+
+#include "common.h"
+#include "chunk.h"
+
+
 
 int main(int argc, const char* argv[]) {
   printf("Hello World\n");
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+  freeChunk(&chunk);
   return 0;
 }
