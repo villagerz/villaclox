816a195cfe36e4bf583648f98351e4c8874e1cce
Villager Z <git.villagerz@pm.me>
2024-06-18 20:13:42 -0400

    added source code line information

--- src/chunk.h
+++ src/chunk.h
@@ -13,12 +13,13 @@ typedef struct {
   int count;
   int capacity;
   uint8_t* code;
+  int* lines;
   ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t bytecode);
+void writeChunk(Chunk* chunk, uint8_t bytecode, int line);
 int addConstant(Chunk* chunkp, Value value);
 
 #endif
