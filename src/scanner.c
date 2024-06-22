147f8ada60c04a01a2daa2925ec7ba785556ac18
Villager Z <git.villagerz@pm.me>
2024-06-22 13:27:53 -0400

    16.3 make string and number tokens

--- src/scanner.c
+++ src/scanner.c
@@ -18,6 +18,10 @@ void initScanner(const char* source) {
   scanner.line = 1;
 }
 
+static bool isDigit(char c) {
+  return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() {
   return *scanner.current == '\0';
 }
@@ -84,6 +88,26 @@ static void skipWhitespace() {
   }
 }
 
+static Token number() {
+  while(isDigit(peek())) advance();
+  if(peek() == '.' && isDigit(peekNext())) {
+    advance();
+    while(isDigit(peek())) advance();
+  }
+  return makeToken(TOKEN_NUMBER);
+}
+	
+
+static Token string() {
+  while(peek() != '"' && !isAtEnd() ) {
+    if (peek() == '\n') scanner.line++;
+    advance();
+  }
+  if (isAtEnd() ) return errorToken("Unterminated string");
+  advance() ; // the closing quote
+  return makeToken(TOKEN_STRING);
+}
+
 static TokenType tryToken(const char c, TokenType use, TokenType elseThis) {
   return match(c) ? use : elseThis;
 }
@@ -94,6 +118,7 @@ Token scanToken() {
   if (isAtEnd() ) makeToken(TOKEN_EOF);
 
   char c = advance();
+  if (isDigit(c)) return number();
   switch(c) {
   case '(': return makeToken(TOKEN_LEFT_PAREN);
   case ')': return makeToken(TOKEN_RIGHT_PAREN);
@@ -110,6 +135,7 @@ Token scanToken() {
   case '=': return makeToken(tryToken('=', TOKEN_EQUAL_EQUAL , TOKEN_EQUAL));
   case '<': return makeToken(tryToken('=', TOKEN_LESS_EQUAL, TOKEN_LESS));
   case '>': return makeToken(tryToken('=', TOKEN_GREATER_EQUAL, TOKEN_GREATER));
+  case '"': return string();
   }
   
   return errorToken("Unexpected characted.");
