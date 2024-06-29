d07fc44b348d6a3ddcd2d1d88f475c58e5ad6e64
Villager Z <git.villagerz@pm.me>
2024-06-29 09:29:27 -0400

    17.4 do prefix parsing and set up precedence

--- src/compiler.c
+++ src/compiler.c
@@ -13,6 +13,21 @@ typedef struct {
   bool panicMode;
 } Parser;
 
+typedef enum {
+  PREC_NONE,
+  PREC_ASSIGNMENT,
+  PREC_OR,
+  PREC_AND,
+  PREC_EQUALITY,
+  PREC_COMPARISON,
+  PREC_TERM,
+  PREC_FACTOR,
+  PREC_UNARY,
+  PREC_CALL,
+  PREC_PRIMARY
+} Precedence;
+
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -77,15 +92,60 @@ static void emitReturn() {
   emitByte(OP_RETURN);
 }
 
+static uint8_t  makeConstant(Value value) {
+  int constant = addConstant(currentChunk(), value);
+  if (constant > UINT8_MAX) {
+    error("Too many constants in one chunk");
+    return 0;
+  }
+
+  return (uint8_t) constant;
+}
+
+static void emitConstant(Value value) {
+  emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
 static void endCompiler() {
   emitReturn();
 }
 
-
-static void expression() {
+static void parsePrecedence(Precedence precedence) {
   return;
 }
 
+
+static void expression() {
+  parsePrecedence(PREC_ASSIGNMENT);
+}
+
+
+// Expression Functions To Add to Function Pointer Array
+
+static void grouping() { // expressions with parenthesis like (123)
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expected ')' after expression.");
+}
+    
+
+static void number() {
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
+
+}
+
+static void unary() {
+  TokenType operatorType = parser.previous.type;
+
+  //compile the operand
+  parsePrecedence(PREC_UNARY);
+
+  //Emit the operator instruction
+  switch(operatorType) {
+  case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+  default: return;
+  }
+}
 
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
