25e0ea9f5a99ffd361fd57617be72d3376381b98
Villager Z <git.villagerz@pm.me>
2024-07-14 21:27:48 -0400

    19.2 struct inhertance starting with Strings as Objs

--- src/object.h
+++ src/object.h
@@ -6,6 +6,10 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+#define AS_STRING(value) ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
+
 typedef enum {
   OBJ_STRING,
 } ObjType;
@@ -19,5 +23,9 @@ struct ObjString {
   int length;
   char* chars;
 };
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
 
 #endif
